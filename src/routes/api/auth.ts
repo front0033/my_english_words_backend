7c40122cbac27245d955ace360842811bdd3bb19
Igor.Bezdenezhnykh <igor.bezdenezhnykh@x5.ru>
2022-01-21 13:00:11 +0300

    added auth models

---
+++ src/routes/api/auth.ts
@@ -0,0 +1,106 @@
+import bcrypt from "bcryptjs";
+import config from "config";
+import { Router, Response } from "express";
+import { check, validationResult } from "express-validator/check";
+import HttpStatusCodes from "http-status-codes";
+import jwt from "jsonwebtoken";
+
+import auth from "../../middleware/auth";
+import Payload from "../../types/Payload";
+import Request from "../../types/Request";
+import User, { IUser } from "../../models/User";
+
+const router: Router = Router();
+
+// @route   GET api/auth
+// @desc    Get authenticated user given the token
+// @access  Private
+router.get("/", auth, async (req: Request, res: Response) => {
+  try {
+    const user: IUser = await User.findById(req.userId).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send("Server Error");
+  }
+});
+
+// @route   POST api/auth
+// @desc    Login user and get token
+// @access  Public
+router.post(
+  "/",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res
+        .status(HttpStatusCodes.BAD_REQUEST)
+        .json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user: IUser = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(HttpStatusCodes.BAD_REQUEST).json({
+          errors: [
+            {
+              msg: "Invalid Credentials",
+            },
+          ],
+        });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(HttpStatusCodes.BAD_REQUEST).json({
+          errors: [
+            {
+              msg: "Invalid Credentials",
+            },
+          ],
+        });
+      }
+
+      const payload: Payload = {
+        userId: user.id,
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: config.get("jwtExpiration") },
+        (err, token) => {
+          if (err) throw err;
+          res
+            .cookie("access_token", token, { httpOnly: true })
+            .json({ userId: user.id, email, avatar: user.avatar });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send("Server Error");
+    }
+  }
+);
+
+// @route   GET api/auth/logout
+// @desc    Logout
+// @access  Private
+router.get("/logout", auth, async (req: Request, res: Response) => {
+  try {
+    res.clearCookie('token');
+    return res.status(200).json({msg: 'logout is OK'});
+  } catch (err) {
+    console.error(err.message);
+    res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send("Server Error");
+  }
+});
+
+export default router;
